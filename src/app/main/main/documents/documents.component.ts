2677c0d64bd05baac5e44e4e82f18b5503ab37df
Guillermo Acosta <guillermo222@att.net>
2021-06-30 16:00:01 -0700

    Creates upload button converting input file to base64

--- src/app/main/main/documents/documents.component.ts
+++ src/app/main/main/documents/documents.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,11 +9,39 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
-  faTrash = faTrash
+  faTrash = faTrash;
+  selectedFile: File
+  toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  }); 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+
+  async onFileChanged(event) {
+    this.selectedFile = await event.target.files[0]
+    this.toBase64(this.selectedFile)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    // console.log(event)
+    // console.log(this.selectedFile)
+    this.http.post('', this.selectedFile)
+      .subscribe(data => {
+        console.log(data);
+      })
+
+  }
+
+
 
 }
